13906f1a8459493bdea469ca1ae7fd229eb66ad6
verica02 <vspaskovska@gmail.com>
2022-06-14 23:13:18 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import {MoralisProvider} from "react-moralis"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <MoralisProvider
+  
+  appId='h9imAhicwLTqse4iPz11XwemAZpjHJFdirfSirmZ' 
+    serverUrl='https://vfl5oefwezfa.usemoralis.com:2053/server'
+  >
+  <Component {...pageProps} />
+  </MoralisProvider>
+  );
 }
 
 export default MyApp
